01398fbc76b73f0612e57ca3de29667de2d91fe6
Kurt Lust <Kurt.Lust@uantwerpen.be>
2021-04-07 18:54:02 +0200

    LUMI and LUMI-C modules for the stack_partition_MOD demo

---
+++ modules/stack_partition_MOD/LUMI-partition.lua
@@ -0,0 +1,32 @@
+family( 'LUMIpartition' )
+
+local partition = myModuleName()
+
+local stack_name =         os.getenv( 'LUMI_STACK_NAME' )
+local stack_version =      os.getenv( 'LUMI_STACK_VERSION' )
+local stack_name_version = os.getenv( 'LUMI_STACK_NAME_VERSION' )
+
+local root = os.getenv( 'LUMITEST_ROOT')
+
+whatis( 'Description: Enables the ' .. partition .. ' binaries for the ' .. stack_name_version .. ' software stack.' )
+
+helpmssg = [==[
+Enables the PARTITION-specific binaries for the STACK software stack.
+
+This module is loaded automatically when loading a software stack based on the
+hardware on which the module load is executed. Replace with a different version
+at your own risk as not all software compiled for one LUMI partition will also
+run on another partition.
+]==]
+helpmssg = helpmssg:gsub( 'PARTITION', partition )
+helpmssg = helpmssg:gsub( 'STACK', stack_name_version )
+help( helpmssg )
+
+setenv( 'LUMI_OVERWRITE_PARTITION', partition )
+
+local stack = stack_name .. '-' .. stack_version
+prepend_path( 'MODULEPATH', pathJoin( root, 'modules', stack, 'Cray' ) )
+prepend_path( 'MODULEPATH', pathJoin( root, 'modules', stack, 'Cray-targets' ) )
+prepend_path( 'MODULEPATH', pathJoin( root, 'modules', stack, partition, 'manual' ) )
+prepend_path( 'MODULEPATH', pathJoin( root, 'modules', stack, partition, 'spack' ) )
+prepend_path( 'MODULEPATH', pathJoin( root, 'modules', stack, partition, 'easybuild' ) )
---
+++ modules/stack_partition_MOD/LUMI/stack.lua
@@ -0,0 +1,27 @@
+family( 'SoftwareStack' )
+
+local partition = os.getenv( 'LUMI_PARTITION' )
+local stack_version = myModuleVersion()
+
+local root = os.getenv( 'LUMITEST_ROOT')
+
+whatis( 'Enables the LUMI-' .. stack_version .. ' software stack for the current partition.' )
+
+local helpmssg = [==[
+This module enables the LUMI-VERSION software stack for the current partition.
+
+By swapping the partition module it is possible to load software compiled for
+a different partition instead, but be careful as that software may not run as
+expected.
+]==]
+helpmssg = helpmssg:gsub( 'VERSION', stack_version )
+help( helpmssg )
+
+local stack = 'LUMI-' .. stack_version
+prepend_path( 'MODULEPATH', pathJoin( root, 'modules', stack, 'LUMIpartition' ) )
+
+setenv( 'LUMI_STACK_NAME',         'LUMI')
+setenv( 'LUMI_STACK_VERSION',      stack_version )
+setenv( 'LUMI_STACK_NAME_VERSION', 'LUMI/' .. stack_version )
+
+load( partition )
