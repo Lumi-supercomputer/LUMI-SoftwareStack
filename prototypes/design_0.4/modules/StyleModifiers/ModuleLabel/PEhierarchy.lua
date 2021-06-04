3d99f37f8beafd0969561bf4e7335734f15142d5
Kurt Lust <Kurt.Lust@uantwerpen.be>
2021-06-04 15:06:06 +0200

    Added an additional, more compact module display style and renamed the previous labeled view.

--- prototypes/design_0.4/modules/StyleModifiers/ModuleLabel/PEhierarchy.lua
+++ prototypes/design_0.4/modules/StyleModifiers/ModuleLabel/PEhierarchy.lua
@@ -4,20 +4,20 @@ end
 
 add_property( 'lmod', 'sticky' )
 
-whatis( 'Enforces showing module directories in the output of module avail rather than descriptitve labels.' )
+whatis( 'Enforces showing descriptitve labels in the output of module avail rather than directories.' )
 
 help( [[
 
 Description
 ===========
 Loading this module sets your environment to tell the module system to use
-module directories rather than descriptive labels for the module categories.
+descriptitve labels rather than directories for the module categories.
 
-With this module loaded, you can still show descriptive labels by using
+With this module loaded, you can still show directories by using
+$  module -s system avail
+instead or get the compact labeled view with the collapsed Cray PE directories
+using
 $  module -s label avail
-instead or get the descriptive labels without collapsing the PE directory
-hierarchy using
-$  module -s PEhierarchy avail
 
 With no ]] .. myModuleName() .. [[ module loaded, you get the defailt behaviour of the
 module tool as configured in the system.
@@ -31,7 +31,7 @@ end
 
 -- Use pushenv to restore the value that a user may have set before when unloading
 -- this module or to restore the value set by the system.
-pushenv( 'LMOD_AVAIL_STYLE', 'label:PEhierarchy:<system>' )
+pushenv( 'LMOD_AVAIL_STYLE', 'label:<PEhierarchy>:system' )
 
 if os.getenv( '_LUMI_LMOD_DEBUG' ) ~= nil then
     LmodMessage( 'DEBUG: ' .. myModuleFullName() .. ', mode ' .. mode() .. ': Exiting' )
--- prototypes/design_0.4/modules/StyleModifiers/ModuleLabel/label.lua
+++ prototypes/design_0.4/modules/StyleModifiers/ModuleLabel/label.lua
@@ -4,18 +4,21 @@ end
 
 add_property( 'lmod', 'sticky' )
 
-whatis( 'Enforces showing descriptitve labels in the output of module avail rather than directories.' )
+whatis( 'Enforces showing descriptitve labels in the output of module avail rather than directories and collapses the Cray PE module hierarchy.' )
 
 help( [[
 
 Description
 ===========
 Loading this module sets your environment to tell the module system to use
-descriptitve labels rather than directories for the module categories.
+descriptitve labels rather than directories for the module categories, and
+it will also collapse the Cray PE module hierarchy to take less space on
+your screen.
 
 With this module loaded, you can still show directories by using
 $  module -s system avail
-instead.
+instead or get the labeled view with the hierarchy by using
+$  module -s PEhierarchy avail
 
 With no ]] .. myModuleName() .. [[ module loaded, you get the defailt behaviour of the
 module tool as configured in the system.
@@ -29,7 +32,7 @@ end
 
 -- Use pushenv to restore the value that a user may have set before when unloading
 -- this module or to restore the value set by the system.
-pushenv( 'LMOD_AVAIL_STYLE', '<label>:system' )
+pushenv( 'LMOD_AVAIL_STYLE', '<label>:PEhierarchy:system' )
 
 if os.getenv( '_LUMI_LMOD_DEBUG' ) ~= nil then
     LmodMessage( 'DEBUG: ' .. myModuleFullName() .. ', mode ' .. mode() .. ': Exiting' )
