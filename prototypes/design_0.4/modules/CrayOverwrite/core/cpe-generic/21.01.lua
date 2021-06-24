1ae1092fb1538986579020e8ca6e224362f9ba44
Kurt Lust <Kurt.Lust@uantwerpen.be>
2021-06-24 09:44:03 +0200

    Made more generic: Full list of Cray PE modules possible, those for which the package is not defined in the CrayPE CSV file will not cause error messages but simply be skipped.

--- prototypes/design_0.4/modules/CrayOverwrite/core/cpe-generic/21.01.lua
+++ prototypes/design_0.4/modules/CrayOverwrite/core/cpe-generic/21.01.lua
@@ -41,11 +41,11 @@ modules = {
     "PrgEnv-cray",
     "PrgEnv-gnu",
     "PrgEnv-intel",
---    "PrgEnv-nvidia",
+    "PrgEnv-nvidia",
     "cce",
     "gcc",
---    "aocc",
---    "nvidia",
+    "aocc",
+    "nvidia",
     "craype",
     "cray-fftw",
     "cray-hdf5",
@@ -60,16 +60,16 @@ modules = {
     "cray-ccdb",
     "cray-cti",
     "cray-dsmml",
---    "cray-jemalloc",
+    "cray-jemalloc",
     "cray-libsci",
     "cray-pmi",
     "cray-pmi-lib",
     "cray-python",
     "cray-stat",
---    "craype-dl-plugin-py3",
+    "craype-dl-plugin-py3",
     "craypkg-gen",
     "gdb4hpc",
---    "iobuf",
+    "iobuf",
     "modules",
     "papi",
     "perftools-base",
@@ -80,12 +80,12 @@ modules = {
 --
 -- Out-commented modules_version are not on the Grenoble system
 modules_version = {}
--- modules_version["PrgEnv-aocc"] =           table_package_version['cpe-prgenv']
+modules_version["PrgEnv-aocc"] =              table_package_version['cpe-prgenv']
 modules_version["PrgEnv-cray"] =              table_package_version['cpe-prgenv']
 modules_version["PrgEnv-gnu"] =               table_package_version['cpe-prgenv']
 modules_version["PrgEnv-intel"] =             table_package_version['cpe-prgenv']
--- modules_version["PrgEnv-nvidia"] =            table_package_version['cpe-prgenv']
--- modules_version["aocc"] =                     table_package_version['AOCC']
+modules_version["PrgEnv-nvidia"] =            table_package_version['cpe-prgenv']
+modules_version["aocc"] =                     table_package_version['AOCC']
 modules_version["atp"] =                      table_package_version['ATP']
 modules_version["cce"] =                      table_package_version['CCE']
 modules_version["cray-R"] =                   table_package_version['cray-R']
@@ -95,7 +95,7 @@ modules_version["cray-dsmml"] =               table_package_version['DSMML']
 modules_version["cray-fftw"] =                table_package_version['FFTW']
 modules_version["cray-hdf5"] =                table_package_version['HDF5']
 modules_version["cray-hdf5-parallel"] =       table_package_version['HDF5']
--- modules_version["cray-jemalloc"] =            table_package_version['jemalloc']
+modules_version["cray-jemalloc"] =            table_package_version['jemalloc']
 modules_version["cray-libsci"] =              table_package_version['LibSci']
 modules_version["cray-libsci_acc"] =          table_package_version['LibSci_acc']
 modules_version["cray-mpich"] =               table_package_version['MPICH']
@@ -105,15 +105,15 @@ modules_version["cray-openshmemx"] =          table_package_version['OpenSMEMX']
 modules_version["cray-parallel-netcdf"] =     table_package_version['parallel-netcdf']
 modules_version["cray-pmi"] =                 table_package_version['PMI']
 modules_version["cray-pmi-lib"] =             table_package_version['PMI']
--- modules_version["cray-python"] =              table_package_version['cray-python']
+modules_version["cray-python"] =              table_package_version['cray-python']
 modules_version["cray-stat"] =                table_package_version['STAT']
 modules_version["craype"] =                   table_package_version['craype']
--- modules_version["craype-dl-plugin-py3"] =     table_package_version['craype-dl-plugin-py3']
+modules_version["craype-dl-plugin-py3"] =     table_package_version['craype-dl-plugin-py3']
 modules_version["craypkg-gen"] =              table_package_version['craypkg-gen']
 modules_version["gcc"] =                      table_package_version['GCC']
 modules_version["gdb4hpc"] =                  table_package_version['gdb4hpc']
--- modules_version["iobuf"] =                    table_package_version['iobuf']
--- modules_version["nvidia"] =                   table_package_version['NVIDIA']
+modules_version["iobuf"] =                    table_package_version['iobuf']
+modules_version["nvidia"] =                   table_package_version['NVIDIA']
 modules_version["papi"] =                     table_package_version['PAPI']
 modules_version["perftools-base"] =           table_package_version['perftools']
 
@@ -131,12 +131,14 @@ if (mode() == "unload") then
 elseif (mode() == "load" or mode() == "show") then
     for _,mod in pairs(modules)
     do
-        if (isloaded(mod)) then
-            if os.getenv( '_LUMI_LMOD_DEBUG' ) ~= nil then
-                LmodMessage( 'DEBUG: ' .. mode() .. ' ' .. myFileName() .. ': Swtiching ' .. mod .. ' to ' .. mod .. '/' .. modules_version[mod] )
+        if modules_version[mod] ~= nil then
+            if (isloaded(mod)) then
+                if os.getenv( '_LUMI_LMOD_DEBUG' ) ~= nil then
+                    LmodMessage( 'DEBUG: ' .. mode() .. ' ' .. myFileName() .. ': Swtiching ' .. mod .. ' to ' .. mod .. '/' .. modules_version[mod] )
+                end
+                unload(mod)
+                load(mod .. "/" .. modules_version[mod])
             end
-            unload(mod)
-            load(mod .. "/" .. modules_version[mod])
         end
     end
 end
