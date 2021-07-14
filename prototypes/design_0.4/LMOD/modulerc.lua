95da69bb8dadc19be8bce72847c8ec9bd0fdc5ba
Kurt Lust <Kurt.Lust@uantwerpen.be>
2021-07-14 11:57:46 +0200

    Update of modulerc.lua to hide some of the cpe modules for which we have alternatives that work better.

--- prototypes/design_0.4/LMOD/modulerc.lua
+++ prototypes/design_0.4/LMOD/modulerc.lua
@@ -6,3 +6,6 @@ module_version( 'ModuleExtensions/show', 'default' )
 module_version( 'ModuleLabel/label', 'default' )
 
 hide_modulefile ( '/opt/cray/pe/lmod/modulefiles/core/cpe/21.04.lua' )
+hide_modulefile ( '/opt/cray/pe/lmod/modulefiles/core/cpe/21.05.lua' )
+hide_modulefile ( '/opt/cray/pe/lmod/modulefiles/core/cpe/21.06.lua' )
+hide_modulefile ( '/opt/cray/pe/lmod/modulefiles/core/cpe/21.07.lua' )
