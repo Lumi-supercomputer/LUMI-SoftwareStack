45bc4e6e5f4e555697f266f39ed7f5017bc421db
Kurt Lust <Kurt.Lust@uantwerpen.be>
2021-12-13 16:17:15 +0100

    Update to the init-lumi module and some other initialisation stuff.

--- modules/init-lumi/0.1.lua
+++ modules/init-lumi/0.1.lua
@@ -1,39 +1,133 @@
+if os.getenv( '_LUMI_LMOD_DEBUG' ) ~= nil then
+    LmodMessage( 'DEBUG: ' .. mode() .. ' ' .. myModuleFullName() .. ': Entering' )
+end
+
+--
+-- Some initialisation
+--
 add_property("lmod","sticky")
 
 -- Find the root of the LUMI installation.
 local LUMI_root = myFileName():match( '(.*)/modules/init%-.*/init%-lumi/.*' )
 local repo =  myFileName():match( '.*/modules/init%-(.*)/init%-lumi/.*' )
 
+--
+-- The actual work
+--
+
 -- Avoid unsetting the following environment variables as they might screw up LMOD.
-if mode() == 'load' or mode() == 'show' then
-
-    -- The place where SiteConfig.lua can be found; it remains to be seen if it is wise
-    -- to change this through a module...
-    setenv( 'LMOD_PACKAGE_PATH', pathJoin( LUMI_root, repo, 'LMOD' ) )
-
-    -- The module properties file, it remains to be seen if it is wise to set this
-    -- through a module...
-    setenv( 'LMOD_RC',           pathJoin( LUMI_root, repo, 'LMOD', 'lmodrc.lua' ) )
-
-    -- The LMOD admin list; it remains to be seen if it is wise to change this through
-    -- a module...
-    setenv( 'LMOD_ADMIN_FILE',   pathJoin( LUMI_root, repo, 'LMOD', 'admin.lst' ) )
-
-end
+-- -- This is not needed if the work is done elsewhere during the initialisation.
+-- if mode() == 'aload' or mode() == 'ashow' then
+--
+--     -- The place where SiteConfig.lua can be found; it remains to be seen if it is wise
+--     -- to change this through a module...
+--     setenv( 'LMOD_PACKAGE_PATH', pathJoin( LUMI_root, repo, 'LMOD' ) )
+--
+--     -- The module properties file, it remains to be seen if it is wise to set this
+--     -- through a module...
+--     setenv( 'LMOD_RC',           pathJoin( LUMI_root, repo, 'LMOD', 'lmodrc.lua' ) )
+--
+--     -- The LMOD admin list; it remains to be seen if it is wise to change this through
+--     -- a module...
+--     setenv( 'LMOD_ADMIN_FILE',   pathJoin( LUMI_root, repo, 'LMOD', 'admin.lst' ) )
+--
+-- end
 
 -- Setting defaults and visibility, note that this is a PATH-style variable
 prepend_path( 'LMOD_MODULERCFILE', pathJoin( LUMI_root, repo, 'LMOD', 'modulerc.lua' ) )
 
 setenv( 'LMOD_AVAIL_STYLE', '<label>:PEhierarchy:system' )
 
+
+
+-- -----------------------------------------------------------------------------
+--
+-- Modules loaded by default
+--
+-- This will only work if the part of code that sets LMOD_PACKAGE_PATH, LMOD_RC
+-- and LMOD_ADMIN_FILE is not needed here but already set during system
+-- initialisation
+--
+
 -- load( 'CrayEnv' )
+-- load( 'LUMI' )
+-- load( 'cpeCray' )
+
+
+-- -----------------------------------------------------------------------------
+--
+-- Enhanced message-of-the-day
+--
+local motd = [[
+
+   .Notes-----------------------------------------------------------.
+   | Make sure one of the following modules is loaded to have the   |
+ * | full LUMI experience:                                          |
+   | - CrayEnv: The Cray software stack enriched with some          |
+   |   additional tools not installed by default in the OS          |
+*  | - LUMI: The extensible LUMI software stack that you can use    |
+   |   as a basis for additional software installs with EasyBuild   |
+ * |   and other tools.                                             |
+   |                                                                |
+   | Please read the following documentation pages to get           |
+   | up-to-date with the changes made in December 2021:             |
+ * | - https://docs.lumi-supercomputer.eu/computing/softwarestacks/ |
+   |   for the basics on LUMI software stack.                       |
+   | - https://docs.lumi-supercomputer.eu/computing/Lmod_modules/   |
+   |   for the basics on the Lmod modules in use since December 15  |
+** |   on LUMI.                                                     |
+** `--*****-------------------***-----------------------*****-------'
+
+]]
+
+if mode() == 'load' or mode() == 'show' then
+
+    if os.getenv( 'LUMI_INIT_FIRST_LOAD' ) == nil then
+
+        -- The problem with LmodMessage is that it does some undesired
+        -- formatting to the output string, replacing multiple spaces
+        -- with single spaces after the initial non-space character.
+        -- Note that LmodMessage itself also writes the result with
+        -- io.stderr:write.
+        -- LmodMessage( motd )
+        if mode() == 'load' then io.stderr:write( motd ) end
+
+        setenv( 'LUMI_INIT_FIRST_LOAD', '1' )
+
+    end
+
+end
+
+
+
+-- -----------------------------------------------------------------------------
+--
+-- Help information
+--
 
 help( [[
+Description
+===========
 The init-lumi module performs most of the initialisations needed to use the
 CrayEnv and LUMI software stacks.
 
+You can load the CrayEnv module to have an enriched Cray programming environment
+or the LUMI module to get the full managed software stack that can be easily
+extended using EasyBuild.
+
 Force-unloading this module will return you to the almost bare Cray LMOD environment
 which you can use without support from LUST at your own risk.
+
+
+More information
+================
+  - See https://docs.lumi-supercomputer.eu/computing/softwarestacks/ for the
+    basics on LUMI software stack.
+  - See https://docs.lumi-supercomputer.eu/computing/Lmod_modules/ for the
+    basics on the Lmod modules in use since December 15 on LUMI
+  - See https://docs.lumi-supercomputer.eu/software/installing/easybuild/ for
+    more information on how to extend the software stack offered by the LUMI
+    module.
 ]] )
 
 whatis( 'init-lumi: Initialisation module for the software stacks. Remove at your own risk.' )
