0d593e7e454cfe992d76db2a93b6565b4dc9551f
Kurt Lust <Kurt.Lust@uantwerpen.be>
2021-12-13 21:06:51 +0100

    Changed the LUST message-of-the-day so that it is read from a file that is easy to replace and added a kind of fortune cookie.

--- LMOD/SitePackage.lua
+++ LMOD/SitePackage.lua
@@ -298,6 +298,69 @@ function get_versionedfile( matching, directory, filenameprefix, filenamesuffix 
 end
 
 
+--
+-- function get_motd
+--
+-- Returns the message-of-the-day in the etc subdirectory of this repository,
+-- or nil if the file is empty or not found.
+--
+function get_motd()
+
+    local LMOD_root = os.getenv( 'LMOD_PACKAGE_PATH' )
+    local motd_file = pathJoin( LMOD_root, '../etc/motd.txt' )
+
+    -- Read the file in a single read statement
+    local fp = io.open( motd_file, 'r' )
+    if fp == nil then return nil end
+    local motd = fp:read( '*all' )
+    fp:close()
+
+    -- Delete trailing white space.
+    motd = motd:gsub( '%s*$', '' )
+
+    -- Return nil if we had an empty file and otherwise return the motd
+    if  #(motd) == 0 then
+        return nil
+    else
+        return motd
+    end
+
+end
+
+
+--
+-- function get_fortune
+--
+-- Returns a "fortune text" readt from etc/lumi_fortune.txt
+--
+function get_fortune()
+
+    local LMOD_root = os.getenv( 'LMOD_PACKAGE_PATH' )
+    local fortune_file = pathJoin( LMOD_root, '../etc/lumi_fortune.txt' )
+
+    -- Read the file in a single read statement
+    local fp = io.open( fortune_file, 'r' )
+    if fp == nil then return nil end
+    local fortune = fp:read( '*all' )
+    fp:close()
+
+    -- Now split up in the blocks of text and delete leading and
+    -- trailing whitespace in each block
+    local separator = '====='
+    local fortune_table = {}
+    for str in string.gmatch( fortune, "([^" .. separator .. "]+)" ) do
+        str = str:gsub( '^%s*', ''  ):gsub( '%s*$', '' )
+        table.insert( fortune_table, str )
+    end
+
+    -- Select a text block (based on the time)
+    -- Indices in LUA arrays start at 1.
+    local fortune_number = os.time() % #(fortune_table) + 1
+
+    return fortune_table[fortune_number] .. '\n'
+
+end
+
 
 
 sandbox_registration{
@@ -307,6 +370,8 @@ sandbox_registration{
     ['get_CPE_component']         = get_CPE_component,
     ['get_CPE_versions']          = get_CPE_versions,
     ['get_versionedfile']         = get_versionedfile,
+    ['get_motd']                  = get_motd,
+    ['get_fortune']               = get_fortune,
 }
 
 
