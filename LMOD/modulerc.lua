c626aacf3d920133db8213d816411230675c5ec4
Kurt Lust <Kurt.Lust@uantwerpen.be>
2021-12-16 19:33:10 +0100

    unhide system cpe modules in CrayEnv and outside every stack as we don't have the overwrite modules anymore.

--- LMOD/modulerc.lua
+++ LMOD/modulerc.lua
@@ -7,10 +7,13 @@ module_version( 'ModuleColour/on', 'default' )
 module_version( 'ModuleExtensions/show', 'default' )
 module_version( 'ModuleLabel/label', 'default' )
 
+if os.getenv( 'LUMI_STACK_NAME' ) ~= nil then
 hide_modulefile ( '/opt/cray/pe/lmod/modulefiles/core/cpe/21.05.lua' )
 hide_modulefile ( '/opt/cray/pe/lmod/modulefiles/core/cpe/21.08.lua' )
 hide_modulefile ( '/opt/cray/pe/lmod/modulefiles/core/cpe/21.09.lua' )
 hide_modulefile ( '/opt/cray/pe/lmod/modulefiles/core/cpe/21.10.lua' )
+hide_modulefile ( '/opt/cray/pe/lmod/modulefiles/core/cpe/21.11.lua' )
+end
 
 -- The following modules do not work with Cray LMOD 8.3.1
 hide_version( 'ModuleExtensions/hide' )
