d1de995ddbbc2c209ccd3e5b771f6e7c4bb50980
Kurt Lust <Kurt.Lust@uantwerpen.be>
2022-02-23 10:30:06 +0100

    Bug fix and making more robust.

--- scripts/onetime/update_add_system_partition.sh
+++ scripts/onetime/update_add_system_partition.sh
@@ -12,8 +12,29 @@ cd ..
 installroot="$(pwd)"
 
 
+###############################################################################
+#
+# create_link
+#
+# Links a file but first tests if the target already exists to avoid error messages.
+#
+# Input arguments:
+#   + First input argument: The target of the link
+#   + Second and mandatory argument: The name of the link
+#
+create_link () {
 
-mkdir -p "$installdir/modules/easybuild/system"
+    test -s "$2" || ln -s "$1" "$2"
+
+}
+
+
+###############################################################################
+#
+# Actual code
+#
+
+mkdir -p "$installroot/modules/easybuild/system"
 mkdir -p "$installroot/modules/manual/system"
 
 mkdir -p "$installroot/SW/system"
@@ -22,26 +43,26 @@ mkdir -p "$installroot/SW/system/MNL"
 
 mkdir -p "$installroot/mgmt/ebrepo_files/system"
 
-pushd "$installdir/modules/SystemPartition/LUMI/21.08/partition"
-ln -s "$installdir/LUMI-SoftwareStack/modules/LUMIpartition/21.01.lua" system.lua
+pushd "$installroot/modules/SystemPartition/LUMI/21.08/partition"
+create_link "$installroot/LUMI-SoftwareStack/modules/LUMIpartition/21.01.lua" system.lua
 popd
 
-mkdir -p "$installdir/modules/Infrastructure/LUMI/21.08/partition/CrayEnv/EasyBuild-production"
-pushd "$installdir/modules/Infrastructure/LUMI/21.08/partition/CrayEnv/EasyBuild-production"
-ln -s "$installdir/LUMI-SoftwareStack/modules/EasyBuild-config/21.01.lua" LUMI.lua
+mkdir -p "$installroot/modules/Infrastructure/LUMI/21.08/partition/CrayEnv/EasyBuild-production"
+pushd "$installroot/modules/Infrastructure/LUMI/21.08/partition/CrayEnv/EasyBuild-production"
+create_link "$installroot/LUMI-SoftwareStack/modules/EasyBuild-config/21.01.lua" LUMI.lua
 cd ..
 /bin/rm -rf EasyBuild-CrayEnv
 popd
 
-mkdir -p "$installdir/modules/Infrastructure/LUMI/21.08/partition/system/EasyBuild-production"
-pushd "$installdir/modules/Infrastructure/LUMI/21.08/partition/system/EasyBuild-production"
-ln -s "$installdir/LUMI-SoftwareStack/modules/EasyBuild-config/21.01.lua" LUMI.lua
+mkdir -p "$installroot/modules/Infrastructure/LUMI/21.08/partition/system/EasyBuild-production"
+pushd "$installroot/modules/Infrastructure/LUMI/21.08/partition/system/EasyBuild-production"
+create_link "$installroot/LUMI-SoftwareStack/modules/EasyBuild-config/21.01.lua" LUMI.lua
 cd ..
 mkdir -p EasyBuild-unlock
 cd EasyBuild-unlock
-ln -s "$installdir/LUMI-SoftwareStack/modules/EasyBuild-unlock/21.01.lua" LUMI.lua
+create_link "$installroot/LUMI-SoftwareStack/modules/EasyBuild-unlock/21.01.lua" LUMI.lua
 cd ..
-ln -s "$installdir/modules/easybuild/LUMI/21.08/partition/common/EasyBuild"
+create_link "$installroot/modules/easybuild/LUMI/21.08/partition/common/EasyBuild"
 popd
 
 
