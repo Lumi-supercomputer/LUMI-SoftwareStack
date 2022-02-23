5bf8e472a3907f99fbda5e0eac23edab3b9eecda
Kurt Lust <Kurt.Lust@uantwerpen.be>
2022-02-23 12:00:30 +0100

    Correcting typos etc.

--- LMOD/modulerc.lua
+++ LMOD/modulerc.lua
@@ -8,13 +8,15 @@ module_version( 'ModuleExtensions/show', 'default' )
 module_version( 'ModuleLabel/label', 'default' )
 
 if os.getenv( 'LUMI_STACK_NAME' ) ~= nil then
-hide_modulefile ( '/opt/cray/pe/lmod/modulefiles/core/cpe/21.05.lua' )
-hide_modulefile ( '/opt/cray/pe/lmod/modulefiles/core/cpe/21.08.lua' )
-hide_modulefile ( '/opt/cray/pe/lmod/modulefiles/core/cpe/21.09.lua' )
-hide_modulefile ( '/opt/cray/pe/lmod/modulefiles/core/cpe/21.10.lua' )
-hide_modulefile ( '/opt/cray/pe/lmod/modulefiles/core/cpe/21.11.lua' )
+hide_modulefile( '/opt/cray/pe/lmod/modulefiles/core/cpe/21.05.lua' )
+hide_modulefile( '/opt/cray/pe/lmod/modulefiles/core/cpe/21.08.lua' )
+hide_modulefile( '/opt/cray/pe/lmod/modulefiles/core/cpe/21.09.lua' )
+hide_modulefile( '/opt/cray/pe/lmod/modulefiles/core/cpe/21.10.lua' )
+hide_modulefile( '/opt/cray/pe/lmod/modulefiles/core/cpe/21.11.lua' )
+hide_modulefile( '/opt/cray/pe/lmod/modulefiles/core/cpe/21.12.lua' )
 end
 
 -- The following modules do not work with Cray LMOD 8.3.1
 hide_version( 'ModuleExtensions/hide' )
 hide_version( 'ModuleExtensions/show' )
+
