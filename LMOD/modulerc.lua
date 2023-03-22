9f69cafa0898e0867255ad9f1fc0823bb97dfc1a
Kurt Lust <Kurt.Lust@uantwerpen.be>
2023-03-22 11:58:09 +0100

    Adaptimg LMOD/modulerc.lua with the list of core/cpe modules.

--- LMOD/modulerc.lua
+++ LMOD/modulerc.lua
@@ -19,15 +19,12 @@ if os.getenv( 'LUMI_LMOD_POWERUSER' ) == nil then
 end
 
 if os.getenv( 'LUMI_STACK_NAME' ) ~= nil then
-hide_modulefile( '/opt/cray/pe/lmod/modulefiles/core/cpe/21.05.lua' )
 hide_modulefile( '/opt/cray/pe/lmod/modulefiles/core/cpe/21.08.lua' )
-hide_modulefile( '/opt/cray/pe/lmod/modulefiles/core/cpe/21.09.lua' )
-hide_modulefile( '/opt/cray/pe/lmod/modulefiles/core/cpe/21.10.lua' )
-hide_modulefile( '/opt/cray/pe/lmod/modulefiles/core/cpe/21.11.lua' )
 hide_modulefile( '/opt/cray/pe/lmod/modulefiles/core/cpe/21.12.lua' )
 hide_modulefile( '/opt/cray/pe/lmod/modulefiles/core/cpe/22.06.lua' )
 hide_modulefile( '/opt/cray/pe/lmod/modulefiles/core/cpe/22.08.lua' )
-hide_modulefile( '/opt/cray/pe/lmod/modulefiles/core/cpe/22.09.lua' )
+hide_modulefile( '/opt/cray/pe/lmod/modulefiles/core/cpe/22.12.lua' )
+hide_modulefile( '/opt/cray/pe/lmod/modulefiles/core/cpe/23.03.lua' )
 end
 
 -- The following modules do not work with Cray LMOD 8.3.1
