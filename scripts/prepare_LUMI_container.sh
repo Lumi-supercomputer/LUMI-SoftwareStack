8bd0dbeaac41745655704ed8a9cdb3524466fc1a
Kurt Lust <Kurt.Lust@uantwerpen.be>
2025-04-22 20:01:26 +0200

    Special prepare script for containers, using symbolic runs but written in a way that it can be run outside the container so that the sanity checks can already use the modules from the software stack.

---
+++ scripts/prepare_LUMI_container.sh
@@ -0,0 +1,140 @@
+#! /bin/bash
+#
+# Script to initialize the LUMI module system.
+#
+# The script takes no arguments.
+#
+# The root of the installation is derived from the place where the script is found.
+# The script should be in <installroot>/${repo}/scripts with <installroot> the
+# root for the installation.
+#
+
+# That cd will work if the script is called by specifying the path or is simply
+# found on PATH. It will not expand symbolic links.
+cd "$(dirname $0)"
+cd ..
+repo="${PWD##*/}"
+cd ..
+installroot="$(pwd)"
+
+#
+# Functions used in this script
+#
+
+create_link () {
+
+#  echo "Linking from: $1"
+#  echo "Linking to: $2"
+#  test -s "$2" && echo "File $2 found."
+#  test -s "$2" || echo "File $2 not found."
+  test -s "$2" || ln -s "$1" "$2"
+
+}
+
+#
+# Create some of the directory structure
+# We use more commands than strictly necessary, which can give more precise
+# error messages.
+#
+mkdir -p "$installroot/modules"
+mkdir -p "$installroot/modules/CrayOverwrite"
+mkdir -p "$installroot/modules/CrayOverwrite/core"
+mkdir -p "$installroot/modules/CrayOverwrite/core/cpe"
+mkdir -p "$installroot/modules/SoftwareStack"
+mkdir -p "$installroot/modules/SoftwareStack/LUMI"     # For the LUMI/yy.mm module files
+mkdir -p "$installroot/modules/SystemPartition"
+mkdir -p "$installroot/modules/SystemPartition/LUMI"   # For LUMI/yy.mm subdirectories
+mkdir -p "$installroot/modules/easybuild"
+mkdir -p "$installroot/modules/easybuild/LUMI"
+mkdir -p "$installroot/modules/easybuild/CrayEnv"
+mkdir -p "$installroot/modules/easybuild/system"
+mkdir -p "$installroot/modules/spack"
+mkdir -p "$installroot/modules/spack/LUMI"
+mkdir -p "$installroot/modules/manual"
+mkdir -p "$installroot/modules/manual/LUMI"
+mkdir -p "$installroot/modules/manual/system"
+mkdir -p "$installroot/modules/Infrastructure"
+mkdir -p "$installroot/modules/Infrastructure/LUMI"
+mkdir -p "$installroot/modules/init-$repo"
+
+mkdir -p "$installroot/SW"
+mkdir -p "$installroot/SW/CrayEnv"
+mkdir -p "$installroot/SW/CrayEnv/EB"
+mkdir -p "$installroot/SW/system"
+mkdir -p "$installroot/SW/system/EB"
+mkdir -p "$installroot/SW/system/MNL"
+
+mkdir -p "$installroot/mgmt"
+mkdir -p "$installroot/mgmt/ebrepo_files"
+mkdir -p "$installroot/mgmt/ebrepo_files/CrayEnv"
+mkdir -p "$installroot/mgmt/ebrepo_files/system"
+mkdir -p "$installroot/mgmt/LMOD"
+mkdir -p "$installroot/mgmt/LMOD/VisibilityHookData"
+mkdir -p "$installroot/mgmt/LMOD/ModuleRC"
+
+mkdir -p "$installroot/sources"
+mkdir -p "$installroot/sources/easybuild"
+mkdir -p "$installroot/sources/easybuild/e"
+mkdir -p "$installroot/sources/easybuild/e/EasyBuild"
+
+#
+# Link the cpe/restore-defaults module
+#
+create_link "../../../../$repo/modules/CrayOverwrite/core/cpe/restore-defaults.lua" \
+            "$installroot/modules/CrayOverwrite/core/cpe/restore-defaults.lua"
+#
+# If the system has a cpe module directory, we must ensure we use the same default
+# version or LMOD may still load from the wrong directory. So we simply link to the
+# Cray .version file but name it .modulerc (it is Tcl) to have a higher priority.
+#
+#cpe_version_file='/opt/cray/pe/lmod/modulefiles/core/cpe/.version'
+#if [ -f "$cpe_version_file" ]
+#then
+#    create_link "$cpe_version_file" "$installroot/modules/CrayOverwrite/core/cpe/.modulerc"
+#fi
+
+#
+# Link the CrayEnv modules
+#
+create_link "../../$repo/modules/CrayEnv.lua"  "$installroot/modules/SoftwareStack/CrayEnv.lua"
+
+#
+# Link the local software collections modules
+#
+for dir in $(/bin/ls -1 $installroot/$repo/modules/LocalStack)
+do
+    create_link "../../$repo/modules/LocalStack/$dir"  "$installroot/modules/SoftwareStack/$dir"
+done
+
+#
+# Check for a Spack setup
+#
+#if [ -d "$installroot/spack/etc/modules/spack" ]
+#then
+#    create_link "../../spack/etc/modules/spack"  "$installroot/modules/SoftwareStack/spack"
+#fi
+
+#
+# Link the style modules
+#
+# We simply link the directory.
+#
+create_link "../../$repo/modules/init-lumi" "$installroot/modules/init-$repo/init-lumi"
+
+#
+# Link the init-lumi module
+#
+# We simply link the directory. The defaults are set in LMOD/modulerc.lua
+#
+create_link "../$repo/modules/StyleModifiers" "$installroot/modules/StyleModifiers"
+
+#
+# Create a modulerc file in the SoftwareStack subdirectory to mark the default software stack.
+# Initialy the default is set to a non-existing module, but we want to create the file.
+#
+if [ ! -s "$installroot/modules/SoftwareStack/LUMI/.modulerc.lua" ]
+then
+cat >"$installroot/modules/SoftwareStack/LUMI/.modulerc.lua" <<EOF
+module_version( "00.00", "default" )
+EOF
+fi
